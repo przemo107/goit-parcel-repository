07827eb5bd6a7762807c63dd44d0371151dcceb9
Przemysław Soliński <solinskip107@gmail.com>
2023-04-20 13:19:21 +0200

    files upload

---
+++ src/js/01-gallery.js
@@ -0,0 +1,38 @@
+import { galleryItems } from './gallery-items.js';
+// Change code below this line
+
+console.log(galleryItems);
+
+function renderGalleryItems() {
+  const gallery = document.querySelector('.gallery');
+  gallery.innerHTML = galleryItems
+    .map(
+      item => `
+      <li class="gallery__item">
+        <a class="gallery__link" href="${item.original}">
+          <img class="gallery__image" src="${item.preview}" data-source="${item.original}" alt="${item.description}">
+        </a>
+      </li>
+    `,
+    )
+    .join('');
+}
+
+renderGalleryItems();
+
+document.querySelector('.gallery').addEventListener('click', event => {
+  event.preventDefault();
+  if (event.target.classList.contains('gallery__image')) {
+    const source = event.target.dataset.source;
+    const instance = basicLightbox.create(`
+      <img src="${event.target.dataset.source}">
+    `);
+    instance.show();
+
+    document.addEventListener('keydown', event => {
+      if (event.key === 'Escape') {
+        instance.close();
+      }
+    });
+  }
+});
