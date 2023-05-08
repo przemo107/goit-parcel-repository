db009464f4c59fc26e689b8d6669389c3bc22c1d
Przemysław Soliński <solinskip107@gmail.com>
2023-05-08 19:26:32 +0200

    video working

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,22 +1,41 @@
+// import Player from '@vimeo/player';
+// const throttle = require('lodash.throttle');
+
+// const iframe = document.querySelector('iframe');
+// const player = new Vimeo.Player(iframe);
+
+// function saveTime(data) {
+//   localStorage.setItem('videoplayer-current-time', data.seconds);
+// }
+
+// player.on('timeupdate', throttle(saveTime, 1000));
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+// const throttle = require('lodash.throttle');
+
+// function saveTime(data) {
+//   localStorage.setItem('videoplayer-current-time', data.seconds);
+// }
+
+// player.on('timeupdate', throttle(saveTime, 1000));
+
+// player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+
 import Player from '@vimeo/player';
-const throttle = require('lodash.throttle');
+import throttle from 'lodash.throttle';
 
 const iframe = document.querySelector('iframe');
-const player = new Vimeo.Player(iframe);
+const player = new Player(iframe);
 
-function saveTime(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+function saveTime() {
+  player.getCurrentTime().then(function (seconds) {
+    localStorage.setItem('videoplayer-current-time', seconds);
+  });
 }
 
 player.on('timeupdate', throttle(saveTime, 1000));
 
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
-const throttle = require('lodash.throttle');
-
-function saveTime(data) {
-  localStorage.setItem('videoplayer-current-time', data.seconds);
+const currentTime = localStorage.getItem('videoplayer-current-time');
+if (currentTime) {
+  player.setCurrentTime(currentTime);
 }
-
-player.on('timeupdate', throttle(saveTime, 1000));
-
-player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
