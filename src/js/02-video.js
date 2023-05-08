bdca670c23234b6370e22b6fe14e1cd013a3e24a
Przemysław Soliński <solinskip107@gmail.com>
2023-05-08 09:58:14 +0200

    video

--- src/js/02-video.js
+++ src/js/02-video.js
@@ -1,7 +1,17 @@
+import Player from '@vimeo/player';
 const throttle = require('lodash.throttle');
 
 const iframe = document.querySelector('iframe');
 const player = new Vimeo.Player(iframe);
+
+function saveTime(data) {
+  localStorage.setItem('videoplayer-current-time', data.seconds);
+}
+
+player.on('timeupdate', throttle(saveTime, 1000));
+
+player.setCurrentTime(localStorage.getItem('videoplayer-current-time'));
+const throttle = require('lodash.throttle');
 
 function saveTime(data) {
   localStorage.setItem('videoplayer-current-time', data.seconds);
